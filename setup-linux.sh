b30f38e96d4fe0c6d70716f673a5ab6e7d14fecd
Jacob Reed <reedjacobt@johndeere.com>
2021-12-14 10:33:14 -0600

    Fix install

--- setup-linux.sh
+++ setup-linux.sh
@@ -35,6 +35,8 @@ echo "Installing basic ZSH plugins"
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
+git clone https://github.com/lukechilds/zsh-better-npm-completion ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-better-npm-completion
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Installing Powerlevel10k ZSH theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
