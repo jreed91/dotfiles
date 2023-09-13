f2651277ccd7df5d23af4eedbbc70b67fee7ffe8
Jacob Reed <jreed91@gmail.com>
2023-09-13 09:39:28 -0500

    Remove docker setup

--- setup-mac.sh
+++ setup-mac.sh
@@ -55,28 +55,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~
 echo "Creating Projects folder inside Home"
 cd ~ && mkdir Projects
 
-echo "Installing Docker & Docker Compose"
-curl -fsSL https://get.docker.com -o ~/Downloads/get-docker.sh
-sudo sh ~/Downloads/get-docker.sh
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-echo "Configuring Docker namespace files"
-sudo rm /etc/subgid && sudo touch /etc/subgid
-sudo rm /etc/subuid && sudo touch /etc/subuid
-echo "$LOGNAME:1000:1" | sudo tee -a /etc/subgid /etc/subuid
-echo "$LOGNAME:100000:65536" | sudo tee -a /etc/subgid /etc/subuid
-
-sudo touch /etc/docker/daemon.json
-echo "
-{
-	\"userns-remap\": \"$LOGNAME\"
-}
-" | sudo tee -a /etc/docker/daemon.json
-
-sudo systemctl restart docker
-
 echo "Done, now you need to logout from your user and log back again"
